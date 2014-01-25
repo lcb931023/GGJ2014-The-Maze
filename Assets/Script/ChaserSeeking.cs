66d622a272567a29dd6a62afec2a78d746551918
Ruofan Zhou <zrfanzy@gmail.com>
2014-01-26 01:34:51 +0800

    .

--- Assets/Script/ChaserSeeking.cs
+++ Assets/Script/ChaserSeeking.cs
@@ -15,10 +15,10 @@ public class ChaserSeeking : MonoBehaviour {
 	void Update () {
 		Vector3 distance = new Vector3 ();
 		distance = Player.transform.position - transform.position;
-		int sx = (int)(transform.position.z / 10);
-		int sy = (int)(transform.position.x / 10);
+		int sx = (int)((transform.position.z) / 10);
+		int sy = (int)((transform.position.x+5) / 10);
 		int tx = (int)(Player.transform.position.z / 10);
-		int ty = (int)(Player.transform.position.x / 10);
+		int ty = (int)((Player.transform.position.x + 5) / 10);
 		if (chaseStarted == false) checkForChaseStarting (distance);
 		chasePlayer (sx, sy, tx, ty, distance);
 	}
@@ -34,104 +34,116 @@ public class ChaserSeeking : MonoBehaviour {
 		if (!chaseStarted) return;
 		GameManager gamemanager = GameManager.ThisClass;
 		MazeGenerator mazeGen = gamemanager.mazeGen;
+		Debug.Log (sx + "," + sy + "to" + tx + "," + ty);
 		ArrayList list = new ArrayList ();
 		ArrayList listans = new ArrayList ();
 		Grid tGrid = new Grid ();
-		tGrid.iCol = sx;
-		tGrid.iRow = sy;
+		tGrid.iRow = sx;
+		tGrid.iCol = sy;
 		list.Add (tGrid);
 		int ans = 0;
 		int start = 0;
 		if (distance.magnitude < 1) return; // die!
-		if (sx == tx && sy == ty) transform.position += distance.normalized * .1f;
+		if (sx.Equals (tx) && sy.Equals (ty)) {
+						transform.position += distance.normalized * .1f;
+						return;
+				}
 		int end = 0;
 		while (start <= end) {
 			tGrid = (Grid)list[start];
 			int direction = 0;
-			int x1 = tGrid.iCol;
-			int y1 = tGrid.iRow;
+			int x1 = tGrid.iRow;
+			int y1 = tGrid.iCol;
+			tGrid = (Grid)mazeGen.indexedGrids[x1 * mazeGen.colNum + y1]; //
 			if (start == 0) direction = 0; else direction = (int)listans[start];
-			//tGrid = (Grid)mazeGen.indexedGrids[x1 * mazeGen.rowNum + y1];
 			if (tGrid.NorthBroken) {
-				transform.position += new Vector3(0.0f, 0.1f, 0.0f);
-				return;
 				Grid sGrid = new Grid();
-				sGrid.iCol = x1 - 1;
-				sGrid.iRow = y1;
-				if (direction == 0) direction = 1;
-				if (sGrid.iCol == tx && sGrid.iRow == ty) {
+				sGrid.iRow = x1 - 1;
+				sGrid.iCol = y1;
+				if (start == 0) direction = 1;
+				if (((sGrid.iRow).Equals(tx)) && (sGrid.iCol.Equals(ty))) {
 					ans = direction;
+					//Debug.Log("N" + tx + ty + sGrid.iRow + sGrid.iCol+ "D" + direction);
 					break;
 				}
-				if (!list.Contains(sGrid) && sGrid.iCol>= 0 && sGrid.iCol < mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow < mazeGen.rowNum) {
+				bool flag = true;
+				for (int i = 0; i < list.Count; ++i)
+					if (((Grid)list[i]).iCol == sGrid.iCol && ((Grid)list[i]).iRow == sGrid.iRow) flag = false;
+				if (flag && sGrid.iCol>= 0 && sGrid.iCol < mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow < mazeGen.rowNum) {
 					list.Add(sGrid);
 					listans.Add (direction);
 					end ++;
 				}
 			}
 			if (tGrid.SouthBroken) {
-				transform.position += new Vector3(0.0f, 0.1f, 0.0f);
-				return;
 				Grid sGrid = new Grid();
-				sGrid.iCol = x1 + 1;
-				sGrid.iRow = y1;
-				if (direction == 0) direction = 2;
-				if (sGrid.iCol == tx && sGrid.iRow == ty) {
+				sGrid.iRow = x1 + 1;
+				sGrid.iCol = y1;
+				if (start == 0) direction = 2;
+				if (((sGrid.iRow).Equals(tx)) && (sGrid.iCol.Equals(ty))) {
 					ans = direction;
+					//Debug.Log("S" + tx + ty + sGrid.iRow + sGrid.iCol+ "D" + direction);
 					break;
 				}
-				if (!list.Contains(sGrid) && sGrid.iCol>= 0 && sGrid.iCol < mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow < mazeGen.rowNum) {
+				bool flag = true;
+				for (int i = 0; i < list.Count; ++i)
+					if (((Grid)list[i]).iCol == sGrid.iCol && ((Grid)list[i]).iRow == sGrid.iRow) flag = false;
+				if (flag && sGrid.iCol>= 0 && sGrid.iCol < mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow < mazeGen.rowNum) {
 					list.Add(sGrid);
 					listans.Add (direction);
 					end ++;
 				}
 			}
 			if (tGrid.EastBroken) {
-				transform.position += new Vector3(0.0f, 0.1f, 0.0f);
-				return;
 				Grid sGrid = new Grid();
-				sGrid.iCol = x1;
-				sGrid.iRow = y1 + 1;
-				list.Add(sGrid);
-				if (sGrid.iCol == tx && sGrid.iRow == ty) {
+				sGrid.iRow = x1;
+				sGrid.iCol = y1 + 1;
+				if (start == 0) direction = 3;
+				if (((sGrid.iRow).Equals(tx)) && (sGrid.iCol.Equals(ty))) {
 					ans = direction;
+					//Debug.Log("E" + tx + ty + sGrid.iRow + sGrid.iCol + "D" + direction);
 					break;
 				}
-				if (!list.Contains(sGrid) && sGrid.iCol>= 0 && sGrid.iCol <= mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow <= mazeGen.rowNum) {
+				bool flag = true;
+				for (int i = 0; i < list.Count; ++i)
+					if (((Grid)list[i]).iCol == sGrid.iCol && ((Grid)list[i]).iRow == sGrid.iRow) flag = false;
+				if (flag && sGrid.iCol>= 0 && sGrid.iCol <= mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow <= mazeGen.rowNum) {
 					list.Add(sGrid);
 					listans.Add (direction);
 					end ++;
 				}
 			}
 			if (tGrid.WestBroken) {
-				transform.position += new Vector3(0.0f, 0.1f, 0.0f);
-				return;
 				Grid sGrid = new Grid();
-				sGrid.iCol = x1;
-				sGrid.iRow = y1 - 1;
-				list.Add(sGrid);
-				if (sGrid.iCol == tx && sGrid.iRow == ty) {
+				sGrid.iRow = x1;
+				sGrid.iCol = y1 - 1;
+				if (start == 0) direction = 4;
+				if (((sGrid.iRow).Equals(tx)) && (sGrid.iCol.Equals(ty))) {
 					ans = direction;
+					//Debug.Log("W" + tx + ty + sGrid.iRow + sGrid.iCol+ "D" + direction);
 					break;
 				}
-				if (!list.Contains(sGrid) && sGrid.iCol>= 0 && sGrid.iCol < mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow < mazeGen.rowNum) {
+				bool flag = true;
+				for (int i = 0; i < list.Count; ++i)
+					if (((Grid)list[i]).iCol == sGrid.iCol && ((Grid)list[i]).iRow == sGrid.iRow) flag = false;
+				if (flag && sGrid.iCol>= 0 && sGrid.iCol < mazeGen.colNum && sGrid.iRow >= 0 && sGrid.iRow < mazeGen.rowNum) {
 					list.Add(sGrid);
 					listans.Add (direction);
 					end ++;
 				}
 			}
-			break;
 			start ++;
 		}
+		Debug.Log ("ans:"+ans);
 		if (ans == 1) {
-			transform.position += new Vector3(0.0f, 0.0f, -0.1f);}
+			transform.position += new Vector3(0.0f, 0.0f, -0.1f);return;}
 		else if (ans == 2) {
-			transform.position += new Vector3(0.0f, 0f, +0.1f); }
+			transform.position += new Vector3(0.0f, 0.0f, +0.1f);return;}
 		else if (ans == 3) {
-			transform.position += new Vector3(0.1f, 0.0f, 0.0f); }
+			transform.position += new Vector3(0.1f, 0.0f, 0.0f);return;}
 		else if (ans ==4) {
-			transform.position += new Vector3(-0.1f, 0.0f, 0.0f); }
-		//else 
-		//	transform.position += distance.normalized * .1f;
+			transform.position += new Vector3(-0.1f, 0.0f, 0.0f);return;}
+		/*else 
+			transform.position += distance.normalized * .1f;*/
 	}
 }
