4dca291b35cd681edf19d0886cdd0375d1265d92
Changbai Li <lcb931023@gmail.com>
2014-01-25 23:04:27 +0800

    .

---
+++ Assets/Script/WalkingAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		animation.CrossFade("idle", 0.25f);  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("w")) {
+			animation.CrossFade ("walk", 0.25f);
+		} else {
+			animation.CrossFade("idle", 0.25f);
+		}
+	}
+}
